8d9652228c73a15d839417ab5c3aad511d235625
p3drac0 <75147146+p3drac0@users.noreply.github.com>
2025-01-28 19:02:53 +0000

    first commit uwu

--- src/models.py
+++ src/models.py
@@ -1,36 +1,56 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey, Integer, String
 from eralchemy2 import render_er
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+app.config["SQLALCHEMY_ECHO"] = True
+db = SQLAlchemy(app)
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(db.Model):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    username = db.Column(db.String(250), nullable=False)
+    firstname = db.Column(db.String(250), nullable=False)
+    lastname = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+class Post(db.Model):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id= db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    user_id = db.Column(db.Integer,ForeignKey('user.id'), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Media(db.Model):
+    __tablename__ = 'media'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    type = db.Column(db.String(250),nullable=False)
+    url = db.Column(db.String(250),nullable=False)
+    post_id=db.Column(db.Integer,ForeignKey('post.id'), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    comment_text = db.Column(db.String(250),nullable=False)
+    author_id = db.Column(db.Integer,ForeignKey('user.id'), nullable=False)
+    post_id=db.Column(db.Integer,ForeignKey('post.id'), nullable=False)
 
+class Follower(db.Model):
+   __tablename__ = 'follower' 
+   user_from_id= db.Column(db.Integer,ForeignKey('user.id'), primary_key=True, nullable=False)
+   user_to_id= db.Column(db.Integer,ForeignKey('user.id'), primary_key=True, nullable=False)
 ## Draw from SQLAlchemy base
 try:
-    result = render_er(Base, 'diagram.png')
+    result = render_er(db.Model, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
